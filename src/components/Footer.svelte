74684ddc66b28e1113487158d36e3314a7430c0b
Captain Overclock <ajmed13@gmail.com>
2023-11-18 21:16:14 -0500

    first draft

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<div id="footer-container">
+    <a href='https://github.com/ajmedeio' target='_blank' rel="noreferrer">
+        <img src="github-logo.png" class="social-logo" alt='github logo'/>
+    </a>
+    <a href='https://www.linkedin.com/in/ajmedeio' target='_blank' rel="noreferrer">
+        <img src="linked-in-logo.png" class="social-logo" alt='linked in logo'/>
+    </a>
+    <a href='https://www.facebook.com/alexander.j.medeiros' target='_blank' rel="noreferrer">
+        <img src="facebook-logo.png" class="social-logo" alt='facebook logo'/>
+    </a>
+</div>
+
+<style>
+    #footer-container {
+        display: 'flex';
+        justify-content: 'center';
+        margin-top: 64px;
+    }
+    .social-logo {
+        width: 32px;
+        margin: 16px;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<nav>
+    <a href="/">About</a>
+    <a href="/portfolio">Portfolio</a>
+    <a href="/contact">Contact</a>
+    <a href="/resume">Resume</a>
+</nav>
+
+<slot></slot>
+
+<script>
+    import Footer from "../components/Footer.svelte"
+</script>
+
+<Footer />
